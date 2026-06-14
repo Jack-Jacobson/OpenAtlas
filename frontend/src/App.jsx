5ee66d6871e6919cd7d89e34b4b3034438411027
jackjacobson <jackjacobson.co@gmail.com>
2026-06-13 20:46:40 -0600

    Framework for login system made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
 import Dashboard from './views/Dashboard.jsx';
+import Login from './views/Login.jsx'
+import { useAuth } from './context/AuthContext.jsx';
 
 export default function App() {
-  const [currentView, setCurrentView] = useState('dashboard');
+  const {isAuthenticated} = useAuth();
 
   return(
-    <div className = "app-root-container">
-      {currentView === 'dashboard' && <Dashboard />}
-      {currentView === 'login' && <div className = "placeholder-portal">Login</div>}
-      {currentView === 'settings' && <div className = "placeholder-portal">Settings</div>}
+    <div className="app-root-container">
+      {isAuthenticated ? <Dashboard /> : <Login />}
     </div>
   );
 }
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -32,7 +32,7 @@ export function AuthProvider ({ children }){
 export function useAuth(){
     const context = useContext(AuthContext);
     if(!context) {
-        throw new Error('useAuth must be used inside an AuthProvider')
+        throw new Error('useAuth must be used inside an AuthProvider');
     }
     return context;
 }
